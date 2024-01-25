31528ef7b8e09489cf590d24c2885c5a358df4cd
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-25 19:31:18 +0100

    Esercizio base finito

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <?php 
@@ -46,16 +47,52 @@
     
         ];
     ?>
-    <ul>
-        <?php 
-        foreach($hotels as $hotels)
-        echo 
-        "<li>"
-        .$hotels["name"]. " è votato ".$hotels["vote"]. " stelle e dista ".$hotels["distance_to_center"]."km dal centro.".
-        "</li>";
-        ?>
+    <!-- Parte iniziale con titolo -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center my-3">
+                <h1>Lista hotels</h1>
+            </div>
 
-    </ul>
+            <!-- Inizio table con parti default -->
+            <table class="table table-striped">
+                <thead>
+                    <tr class="table-success">
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th class="text-center" scope="col">voto</th>
+                        <th class="text-center" scope="col">Distanza dal centro</th>
+                        <th class="text-center" scope="col">Parcheggio</th>
+                    </tr>
+                </thead>
 
+                <!-- Inizio corpo centrale con for each su Hotels -->
+               <tbody>
+                <?php
+                    foreach($hotels as $hotels){
+
+                    // if & else per trasformare parking da true in si e false in no
+                    if($hotels["parking"] === true){
+                        $hotels["parking"] = "si";
+                    }
+                    else{
+                        $hotels["parking"] = "no";
+                    }
+
+                    // struttura base per il foreach che riciclerà
+                    echo
+                    '<tr class="my-5">
+                        <th scope="col">'.$hotels["name"].'</th>
+                        <th scope="col">'.$hotels["description"].'</th>
+                        <th class="text-center" scope="col">'.$hotels["vote"].'</th>
+                        <th class="text-center" scope="col">'.$hotels["distance_to_center"].'km</th>
+                        <th class="text-center" scope="col">'.$hotels["parking"].'</th>
+                    </tr>';
+                    }
+                ?>
+               </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
