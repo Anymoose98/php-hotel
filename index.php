72f05bfc662670d006aa2946d8c7a5402f9c51d7
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-26 09:40:33 +0100

    ultimo push di ieri sera che ho scordato

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <?php 
@@ -92,6 +93,39 @@
                 ?>
                </tbody>
             </table>
+        </div>
+    </div>
+
+    <div class="bg-success my-5 text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 text-center my-3">
+                    <ul>
+                        <li class="my-2">Paesi</li>
+                        <li class="my-2">Aree geografiche</li>
+                        <li class="my-2">Città</li>
+                        <li class="my-2">Quartieri</li>
+                    </ul>
+                </div>
+
+                <div class="col-4 text-center my-3">
+                    <ul>
+                        <li class="my-2">Case</li>
+                        <li class="my-2">Appartamenti</li>
+                        <li class="my-2">Resort</li>
+                        <li class="my-2">Ville</li>
+                    </ul>
+                </div>
+
+                <div class="col-4 text-center my-3">
+                    <ul>
+                        <li class="my-2">Autonoleggio</li>
+                        <li class="my-2">Ricerca voli</li>
+                        <li class="my-2">Prenotazione ristorante</li>
+                        <li class="my-2">Offerte generali</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </body>
